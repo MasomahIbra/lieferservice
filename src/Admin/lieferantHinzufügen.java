d1cc305175791b6f376b4360f0c1913d0db1bcbd
Masomah Ibrahimi <masomah01@gmail.com>
2020-01-29 09:11:39 +0100

    Lieferando

--- src/Admin/lieferantHinzufügen.java
+++ src/Admin/lieferantHinzufügen.java
@@ -1,42 +1,49 @@
 package Admin;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+public class lieferantHinzufügen {
 
-public class lieferantHinzufügen extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					lieferantHinzufügen frame = new lieferantHinzufügen();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+	
+		try {
+			// Datenbankklasse dynamisch erzeugen.
+			Class.forName ("org.sqlite.JDBC");
+
+			// Verbindung initialisieren.
+			String datei = "Lieferant.db";
+			String url = "jdbc:sqlite:" + datei;
+			Connection conn =  DriverManager.getConnection (url);
+
+			// Erzeugen einer SQL-Anweisung.
+			String sql = "SELECT * from Lieferant where Kategorie ";
+			PreparedStatement statement = conn.prepareStatement (sql);
+			
+
+			// SQL-Anweisung ausführen.
+			ResultSet resultSet = statement.executeQuery();
+			
+			//  Daten auslesen Datensatzzeiger auf den ersten Datensatz setzen.
+			while(resultSet.next()) { 
+				// Daten auslesen.
+				String text = resultSet.getString("Kategorie");
+				text  +=", " + resultSet.getString("Art");
+				text +=", " + resultSet.getString("Food");
+				text +=", " + resultSet.getString("Groesse");
+				text +=", " + resultSet.getString("Preis");
+				System.out.println(text);
+				
 			}
-		});
+	} catch (ClassNotFoundException er) {
+		er.printStackTrace();
+	} catch (SQLException er) {
+		// TODO Auto-generated catch block
+		er.printStackTrace();
+		}
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public lieferantHinzufügen() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
-
 }
+
