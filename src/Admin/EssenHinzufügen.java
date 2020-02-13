e4bf23d4505dd26e7e7c6fba0f029be18a738e4e
Masomah Ibrahimi <masomah01@gmail.com>
2020-02-13 09:09:30 +0100

    neue

--- src/Admin/EssenHinzufügen.java
+++ src/Admin/EssenHinzufügen.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class lieferantHinzufügen {
+public class EssenHinzufügen {
 
 	public static void main(String[] args) {
 	
