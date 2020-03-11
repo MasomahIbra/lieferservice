2b2017dc145bf55979dc4e69221b1c8f1235c655
Masomah Ibrahimi <masomah01@gmail.com>
2020-03-11 09:02:33 +0100

    AnmeldenBenutzer

---
+++ ProjectPP/lieferservice/LieferService.java
@@ -0,0 +1,31 @@
+package lieferservice;
+
+import javax.swing.JFrame;
+
+import lieferservice.gui.MainJFrame;
+
+
+/**
+ * Diese Klasse enthält die main-Methode, über die das Programm gestartet wird.
+ * @author MasoumehIbrahimi
+ *
+ */
+public class LieferService {
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainJFrame frame = new MainJFrame();
+
+					frame.setVisible(true);
+					frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
