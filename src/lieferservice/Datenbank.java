34b092db4c02b84b712bb576eaa1bb5f60ca81fd
Masomah Ibrahimi <masomah01@gmail.com>
2020-03-11 09:24:38 +0100

    Benutzer

---
+++ src/lieferservice/Datenbank.java
@@ -0,0 +1,80 @@
+package lieferservice;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Datenbank {
+
+	static Connection conn = null;
+	
+	
+	/**
+	 * Diese Methode öffnet die Datenbank.
+	 * Wenn ein Fehler auftritt, wird das Programm beendet.
+	 */
+	private static void oeffneDatenbank() {
+		try {
+			// Datenbankklasse dynamisch erzeugen.
+			Class.forName ("org.sqlite.JDBC");
+
+			// Verbindung initialisieren.
+			String datei = "pp.db";
+			String url = "jdbc:sqlite:" + datei;
+			conn =  DriverManager.getConnection (url);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	
+	/**
+	 * Diese Methode führt eine Datenbankabfrage durch.
+	 * 
+	 * @param sql SQL-Anweisung mit der Datenbankabfrage
+	 * @throws SQLException, falls ein Fehler auftritt
+	 */
+	public static ResultSet abfragen(String sql) throws SQLException{
+		// Falls das noch nicht passiert ist, öffnen wir die Datenbank
+		if (conn==null) oeffneDatenbank();
+		
+		// SQL Abfrage durchführen
+		PreparedStatement statement = conn.prepareStatement (sql);
+		ResultSet resultSet = statement.executeQuery();	
+		
+		return resultSet;
+	}
+	
+	/*
+			// Erzeugen einer SQL-Anweisung.
+
+			
+			//  Daten auslesen Datensatzzeiger auf den ersten Datensatz setzen.
+			while(resultSet.next()) { 
+				// Daten auslesen.
+				String text = resultSet.getString("id");
+				text  +=", " + resultSet.getString("Vorname");
+				text +=", " + resultSet.getString("ID");
+				text +=", " + resultSet.getString("Geburtsdatum");
+				text +=", " + resultSet.getString("Fuehrerschein");
+				text +=", " + resultSet.getString("Ort");
+				text +=", " + resultSet.getString("Hausnummer");
+				text +=", " +  resultSet.getString("Eigener_Kunde");
+				System.out.println(text);
+				
+			}
+	} catch (ClassNotFoundException er) {
+		er.printStackTrace();
+	} catch (SQLException er) {
+		// TODO Auto-generated catch block
+		er.printStackTrace();
+		}
+	}
+	*/
+}
